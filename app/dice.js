f3ba92bb3420cf7760904b5a60e89fb4908980bc
wertrain <wertrain@gmail.com>
2016-05-28 19:08:52 +0900

    shader compile and link

--- app/dice.js
+++ app/dice.js
@@ -21,33 +21,7 @@ var webgldice = {};
         this.gl.clearColor(r, g, b, 1.0);
         this.gl.clear(this.gl.COLOR_BUFFER_BIT);
     };
-    SimpleGL.prototype.createShader = function(id) {
-        var scriptElement = document.getElementById(id);
-        if(!scriptElement) {
-            console.log('createShader: "' + id + '" not found.');
-            return null;
-        }
-        
-        var shader = null;
-        switch(scriptElement.type) {
-            case 'x-shader/x-vertex':
-                shader = this.gl.createShader(this.gl.VERTEX_SHADER);
-                break;
-            case 'x-shader/x-fragment':
-                shader = this.gl.createShader(this.gl.FRAGMENT_SHADER);
-                break;
-            default:
-                return null;
-        }
-        this.gl.shaderSource(shader, scriptElement.text);
-        this.gl.compileShader(shader);
-        if (!this.gl.getShaderParameter(shader, this.gl.COMPILE_STATUS)) {
-            console.log('compile error.');
-            return null;
-        }
-        return shader;
-    };
-    SimpleGL.prototype.loadShader = function(url, data, successCallback, errorCallback) {
+    SimpleGL.prototype.loadFile = function(url, data, successCallback, errorCallback) {
         var request = new XMLHttpRequest();
         request.open('GET', url, true);
         request.onreadystatechange = function () {
@@ -61,15 +35,66 @@ var webgldice = {};
         };
         request.send(null);
     }
+    SimpleGL.prototype.compileShader = function(type, text) {
+        var shader = null;
+        switch(type) {
+            case 0: // 'x-shader/x-vertex'
+                shader = this.gl.createShader(this.gl.VERTEX_SHADER);
+                break;
+            case 1: // 'x-shader/x-fragment'
+                shader = this.gl.createShader(this.gl.FRAGMENT_SHADER);
+                break;
+            default:
+                return null;
+        }
+        this.gl.shaderSource(shader, text);
+        this.gl.compileShader(shader);
+        if (!this.gl.getShaderParameter(shader, this.gl.COMPILE_STATUS)) {
+            console.log('compile error.');
+            return null;
+        }
+        return shader;
+    };
+    SimpleGL.prototype.linkProgram = function(vs, fs) {
+        var program = this.gl.createProgram();
+
+        this.gl.attachShader(program, vs);
+        this.gl.attachShader(program, fs);
+        this.gl.linkProgram(program);
+        
+        if(!this.gl.getProgramParameter(program, this.gl.LINK_STATUS)) {
+            console.log('link error:' + this.gl.getProgramInfoLog(program));
+            return null;
+        }
+        return program;
+    };
+    SimpleGL.prototype.createShaders = function(shaderFileUrls, successCallback, errorCallback) {
+        var compiled = 0;
+        var completed = shaderFileUrls.length;
+        var shaders = [];
+        var that = this;
+        
+        function innerCallback(response, index) {
+            shaders[index] = that.compileShader(index, response);
+            if (shaders[index] === null) {
+                errorCallback();
+            } else if (++compiled === completed) {
+                var program = that.linkProgram(shaders[0], shaders[1]);
+                that.gl.useProgram(program);
+                successCallback();
+            }
+        }
+        for (var i = 0; i < shaderFileUrls.length; ++i) {
+            this.loadFile(shaderFileUrls[i], i, innerCallback);
+        }
+    }
     webgldice.SimpleGL = SimpleGL;
 }());
 
 var main = function() {
     var sgl = new webgldice.SimpleGL();
     sgl.initalize('canvas', 640, 480);
-    sgl.clear(0.0, 0.0, 255.0);
-    sgl.loadShader('/shaders/vertex.vs', null, function(response, data) {
-        
+    sgl.createShaders(['/shaders/vertex.vs', '/shaders/fragment.fs'], function() {
+        sgl.clear(0.0, 0.0, 255.0);
     });
-    //sgl.loadShader('/shaders/fragment.fs');
 }();
