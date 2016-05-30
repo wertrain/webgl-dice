5f51f2dcf4f0ddadb981d78fbd13fc8bfbeb6d57
wertrain <wertrain@gmail.com>
2016-05-31 00:05:08 +0900

    refactor

--- app/dice.js
+++ app/dice.js
@@ -28,14 +28,36 @@ var webgldice = {};
         request.onreadystatechange = function () {
             if (request.readyState == 4) {
                 if (request.status == 200) {
-                    successCallback(request.responseText, data)
+                    successCallback(request.responseText, data);
                 } else {
                     errorCallback(url);
                 }
             }
         };
         request.send(null);
-    }
+    };
+    SimpleGL.prototype.loadFiles = function(urls, data, successCallback, errorCallback) {
+        var allItemCount = urls.length;
+        var files = new Array(allItemCount);
+        var count = 0;
+        for (var i = 0; i < urls.length; ++i) {
+            var request = new XMLHttpRequest();
+            request.open('GET', urls[i], true);
+            request.onreadystatechange = function () {
+                if (request.readyState == 4) {
+                    if (request.status == 200) {
+                        files[i] = request.responseText;
+                        if (++count === allItemCount) {
+                            successCallback(files, data);
+                        }
+                    } else {
+                        errorCallback(urls[i]);
+                    }
+                }
+            };
+            request.send(null);
+        }
+    };
     SimpleGL.prototype.compileShader = function(type, text) {
         var shader = null;
         switch(type) {
@@ -92,7 +114,7 @@ var webgldice = {};
         return texture;
     };
     SimpleGL.prototype.createShaders = function(shaderFileUrls, successCallback, errorCallback) {
-        var compiled = 0;
+        var current = 0;
         var completed = shaderFileUrls.length;
         var shaders = [];
         var that = this;
@@ -101,7 +123,7 @@ var webgldice = {};
             shaders[index] = that.compileShader(index, response);
             if (shaders[index] === null) {
                 errorCallback();
-            } else if (++compiled === completed) {
+            } else if (++current === completed) {
                 var program = that.linkProgram(shaders[0], shaders[1]);
                 that.gl.useProgram(program);
                 successCallback(shaders[0], shaders[1], program);
@@ -111,6 +133,28 @@ var webgldice = {};
             this.loadFile(shaderFileUrls[i], i, innerCallback, function(url){
                 console.log('Failed to load: ' + url);
             });
+        }
+    };
+    SimpleGL.prototype.loadImages = function(imageFileUrls, successCallback, errorCallback) {
+        function collector(allItemCount, callback, param) {
+            var loadedCount = 0;
+            return function() {
+                if(++loadedCount == allItemCount) {
+                    callback(param);
+                }
+            }
+        }
+        function error() {
+            errorCallback(this.src);
+        }
+        var images = new Array(imageFileUrls.length);
+        var success = new collector(imageFileUrls.length, successCallback, images);
+        for (var i = 0; i < imageFileUrls.length; ++i) {
+            var image = new Image();
+            image.src = imageFileUrls[i];
+            image.onload = success;
+            image.onerror = error;
+            images[i] = image;
         }
     };
     SimpleGL.prototype.getGL = function() {
@@ -128,126 +172,123 @@ var webgldice = {};
 var main = function() {
     var sgl = new webgldice.SimpleGL();
     sgl.initalize('canvas', 640, 480);
-    
-    sgl.createShaders(['/shaders/vertex.vs', '/shaders/fragment.fs'], function(vs, fs, program) {
-        var gl = sgl.getGL();
-        var attLocation = new Array(3);
-        attLocation[0] = gl.getAttribLocation(program, 'position');
-        attLocation[1] = gl.getAttribLocation(program, 'color');
-        attLocation[2] = gl.getAttribLocation(program, 'textureCoord');
-        var attStride = new Array(3);
-        attStride[0] = 3;
-        attStride[1] = 4;
-        attStride[2] = 2;
-        var vertexPosition = [
-            // Front face
-            -1.0, -1.0,  1.0,
-             1.0, -1.0,  1.0,
-             1.0,  1.0,  1.0,
-            -1.0,  1.0,  1.0,
-            // Back face
-            -1.0, -1.0, -1.0,
-            -1.0,  1.0, -1.0,
-             1.0,  1.0, -1.0,
-             1.0, -1.0, -1.0,
-            // Top face
-            -1.0,  1.0, -1.0,
-            -1.0,  1.0,  1.0,
-             1.0,  1.0,  1.0,
-             1.0,  1.0, -1.0,
-            // Bottom face
-            -1.0, -1.0, -1.0,
-             1.0, -1.0, -1.0,
-             1.0, -1.0,  1.0,
-            -1.0, -1.0,  1.0,
-            // Right face
-             1.0, -1.0, -1.0,
-             1.0,  1.0, -1.0,
-             1.0,  1.0,  1.0,
-             1.0, -1.0,  1.0,
-            // Left face
-            -1.0, -1.0, -1.0,
-            -1.0, -1.0,  1.0,
-            -1.0,  1.0,  1.0,
-            -1.0,  1.0, -1.0
-        ];
-        var colors = [
-            [1.0,  1.0,  1.0,  1.0],    // Front face: white
-            [1.0,  1.0,  1.0,  1.0],    // Back face: white
-            [1.0,  1.0,  1.0,  1.0],    // Top face: white
-            [1.0,  1.0,  1.0,  1.0],    // Bottom face: white
-            [1.0,  1.0,  1.0,  1.0],    // Right face: white
-            [1.0,  1.0,  1.0,  1.0]     // Left face: white
-        ];
-        var vertexColor = [];
-        for (var j = 0; j < colors.length; ++j) {
-            var c = colors[j];
-            for (var i = 0; i < colors[j].length; ++i) {
-                vertexColor = vertexColor.concat(c);
+    sgl.loadImages(['/textures/dice.png'], function(images) {
+        sgl.createShaders(['/shaders/vertex.vs', '/shaders/fragment.fs'], function(vs, fs, program) {
+            var gl = sgl.getGL();
+            var attLocation = new Array(3);
+            attLocation[0] = gl.getAttribLocation(program, 'position');
+            attLocation[1] = gl.getAttribLocation(program, 'color');
+            attLocation[2] = gl.getAttribLocation(program, 'textureCoord');
+            var attStride = new Array(3);
+            attStride[0] = 3;
+            attStride[1] = 4;
+            attStride[2] = 2;
+            var vertexPosition = [
+                // Front face
+                -1.0, -1.0,  1.0,
+                 1.0, -1.0,  1.0,
+                 1.0,  1.0,  1.0,
+                -1.0,  1.0,  1.0,
+                // Back face
+                -1.0, -1.0, -1.0,
+                -1.0,  1.0, -1.0,
+                 1.0,  1.0, -1.0,
+                 1.0, -1.0, -1.0,
+                // Top face
+                -1.0,  1.0, -1.0,
+                -1.0,  1.0,  1.0,
+                 1.0,  1.0,  1.0,
+                 1.0,  1.0, -1.0,
+                // Bottom face
+                -1.0, -1.0, -1.0,
+                 1.0, -1.0, -1.0,
+                 1.0, -1.0,  1.0,
+                -1.0, -1.0,  1.0,
+                // Right face
+                 1.0, -1.0, -1.0,
+                 1.0,  1.0, -1.0,
+                 1.0,  1.0,  1.0,
+                 1.0, -1.0,  1.0,
+                // Left face
+                -1.0, -1.0, -1.0,
+                -1.0, -1.0,  1.0,
+                -1.0,  1.0,  1.0,
+                -1.0,  1.0, -1.0
+            ];
+            var colors = [
+                [1.0,  1.0,  1.0,  1.0],    // Front face: white
+                [1.0,  1.0,  1.0,  1.0],    // Back face: white
+                [1.0,  1.0,  1.0,  1.0],    // Top face: white
+                [1.0,  1.0,  1.0,  1.0],    // Bottom face: white
+                [1.0,  1.0,  1.0,  1.0],    // Right face: white
+                [1.0,  1.0,  1.0,  1.0]     // Left face: white
+            ];
+            var vertexColor = [];
+            for (var j = 0; j < colors.length; ++j) {
+                var c = colors[j];
+                for (var i = 0; i < colors[j].length; ++i) {
+                    vertexColor = vertexColor.concat(c);
+                }
             }
-        }
-        var textureCoord = [
-            0.0, 1.0,
-            0.0, 0.0,
-            0.125, 0.0,
-            0.125, 1.0,
-            
-            0.625, 1.0,
-            0.625, 0.0,
-            0.75, 0.0,
-            0.75, 1.0,
-            
-            0.5, 1.0,
-            0.5, 0.0,
-            0.625, 0.0,
-            0.625, 1.0,
-            
-            0.125, 1.0,
-            0.125, 0.0,
-            0.25, 0.0,
-            0.25, 1.0,
-            
-            0.25, 1.0,
-            0.25, 0.0,
-            0.375, 0.0,
-            0.375, 1.0,
-            
-            0.375, 1.0,
-            0.375, 0.0,
-            0.5, 0.0,
-            0.5, 1.0,
-        ];
-        var index = [
-            0,  1,  2,      0,  2,  3,    // front
-            4,  5,  6,      4,  6,  7,    // back
-            8,  9,  10,     8,  10, 11,   // top
-            12, 13, 14,     12, 14, 15,   // bottom
-            16, 17, 18,     16, 18, 19,   // right
-            20, 21, 22,     20, 22, 23    // left
-        ];
-        var pvbo = sgl.createVBO(vertexPosition);
-        gl.bindBuffer(gl.ARRAY_BUFFER, pvbo);
-        gl.enableVertexAttribArray(attLocation[0]);
-        gl.vertexAttribPointer(attLocation[0], attStride[0], gl.FLOAT, false, 0, 0);
-        var cvbo = sgl.createVBO(vertexColor);
-        gl.bindBuffer(gl.ARRAY_BUFFER, cvbo);
-        gl.enableVertexAttribArray(attLocation[1]);
-        gl.vertexAttribPointer(attLocation[1], attStride[1], gl.FLOAT, false, 0, 0);
-        var tvbo = sgl.createVBO(textureCoord);
-        gl.bindBuffer(gl.ARRAY_BUFFER, tvbo);
-        gl.enableVertexAttribArray(attLocation[2]);
-        gl.vertexAttribPointer(attLocation[2], attStride[2], gl.FLOAT, false, 0, 0);
-        var ibo = sgl.createIBO(index);
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
+            var textureCoord = [
+                0.0, 1.0,
+                0.0, 0.0,
+                0.125, 0.0,
+                0.125, 1.0,
+                
+                0.625, 1.0,
+                0.625, 0.0,
+                0.75, 0.0,
+                0.75, 1.0,
+                
+                0.5, 1.0,
+                0.5, 0.0,
+                0.625, 0.0,
+                0.625, 1.0,
+                
+                0.125, 1.0,
+                0.125, 0.0,
+                0.25, 0.0,
+                0.25, 1.0,
+                
+                0.25, 1.0,
+                0.25, 0.0,
+                0.375, 0.0,
+                0.375, 1.0,
+                
+                0.375, 1.0,
+                0.375, 0.0,
+                0.5, 0.0,
+                0.5, 1.0,
+            ];
+            var index = [
+                0,  1,  2,      0,  2,  3,    // front
+                4,  5,  6,      4,  6,  7,    // back
+                8,  9,  10,     8,  10, 11,   // top
+                12, 13, 14,     12, 14, 15,   // bottom
+                16, 17, 18,     16, 18, 19,   // right
+                20, 21, 22,     20, 22, 23    // left
+            ];
+            var pvbo = sgl.createVBO(vertexPosition);
+            gl.bindBuffer(gl.ARRAY_BUFFER, pvbo);
+            gl.enableVertexAttribArray(attLocation[0]);
+            gl.vertexAttribPointer(attLocation[0], attStride[0], gl.FLOAT, false, 0, 0);
+            var cvbo = sgl.createVBO(vertexColor);
+            gl.bindBuffer(gl.ARRAY_BUFFER, cvbo);
+            gl.enableVertexAttribArray(attLocation[1]);
+            gl.vertexAttribPointer(attLocation[1], attStride[1], gl.FLOAT, false, 0, 0);
+            var tvbo = sgl.createVBO(textureCoord);
+            gl.bindBuffer(gl.ARRAY_BUFFER, tvbo);
+            gl.enableVertexAttribArray(attLocation[2]);
+            gl.vertexAttribPointer(attLocation[2], attStride[2], gl.FLOAT, false, 0, 0);
+            var ibo = sgl.createIBO(index);
+            gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
 
-        var uniLocation = new Array();
-        uniLocation[0]  = gl.getUniformLocation(program, 'mvpMatrix');
-        uniLocation[1]  = gl.getUniformLocation(program, 'texture');
-        
-        var diceImage = new Image();
-        diceImage.src = '/textures/dice.png';
-        diceImage.onload = function() {
-            var texture = sgl.createTexture(diceImage);
+            var uniLocation = new Array();
+            uniLocation[0]  = gl.getUniformLocation(program, 'mvpMatrix');
+            uniLocation[1]  = gl.getUniformLocation(program, 'texture');
+            
+            var texture = sgl.createTexture(images[0]);
             var count = 0;
             var minMatrix = new matIV();
             var mtxView = minMatrix.identity(minMatrix.create());
@@ -263,7 +304,7 @@ var main = function() {
                 minMatrix.rotate(mtxModel, rad, [0, 1, 1], mtxModel);
                 minMatrix.multiply(mtxProj, mtxView, mtxMVP);
                 minMatrix.multiply(mtxMVP, mtxModel, mtxMVP);
- 
+
                 gl.clearColor(0.0, 0.0, 0.0, 1.0);
                 gl.clearDepth(1.0);
                 gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
@@ -278,6 +319,6 @@ var main = function() {
                 gl.flush();
                 setTimeout(arguments.callee, 1000 / 30);
             })();
-        }
+        });
     });
 }();
